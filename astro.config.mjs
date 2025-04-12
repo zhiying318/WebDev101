6f0a54ac04b93c1193afd1f1f38f27afa49bd799
Zhiying Zou <zhiying.zou@telecom-paris.fr>
2025-04-13 01:21:22 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://zhiying318.github.io',
+    base: '/WebDev101/', // 重要！适配 GitHub Pages 子路径
+});
